a3f23de60eb55c607a81cb3758132a4c0e4ca1d7
Ahmed Mohammed Alghamdi <aalghamdi4601@stu.kau.edu.sa>
2026-06-05 14:47:22 +0300

    Add users and students for multiple departments
    Added multiple student and faculty entries for various departments.

--- add_test_user.py
+++ add_test_user.py
@@ -3,9 +3,44 @@ from database import DatabaseManager
 from auth import hash_password
 
 db = DatabaseManager()
-db.add_user("test@uni.edu", hash_password("Password1"), "student")
-db.add_student("S001", "Test Student", 3.5, "Computer", ["COE301"], user_email="test@uni.edu")
-print("Done!")
 
+# Faculty
 db.add_user("faculty@uni.edu", hash_password("Password1"), "faculty")
-db.add_faculty("F001", "Dr. Ahmed", "ECE", user_email="faculty@uni.edu")
+db.add_faculty("F001", "Dr. Khalid Al-Rashidi", "ECE", user_email="faculty@uni.edu")
+
+# Computer Engineering
+db.add_user("s001@uni.edu", hash_password("Password1"), "student")
+db.add_student("S001", "Nasser Al-Otaibi", 4.2, "Computer",
+    ["EE301", "EE360", "EE250", "EE300"],
+    user_email="s001@uni.edu")
+
+db.add_user("s002@uni.edu", hash_password("Password1"), "student")
+db.add_student("S002", "Badr Al-Qahtani", 3.8, "Computer",
+    ["EE301", "EE300", "EE367", "EE250"],
+    user_email="s002@uni.edu")
+
+db.add_user("s003@uni.edu", hash_password("Password1"), "student")
+db.add_student("S003", "Rayan Al-Zahrani", 3.9, "Computer",
+    ["EE301", "EE360", "EE250", "EE321"],
+    user_email="s003@uni.edu")
+
+# Communications
+db.add_user("s004@uni.edu", hash_password("Password1"), "student")
+db.add_student("S004", "Faisal Al-Harbi", 3.8, "Communications",
+    ["EE301", "EE321", "EE421", "EE250"],
+    user_email="s004@uni.edu")
+
+# Power
+db.add_user("s005@uni.edu", hash_password("Password1"), "student")
+db.add_student("S005", "Tariq Al-Shehri", 4.1, "Power",
+    ["EE301", "EE351", "EE451", "EE250"],
+    user_email="s005@uni.edu")
+
+# Biomedical
+db.add_user("s006@uni.edu", hash_password("Password1"), "student")
+db.add_student("S006", "Hamad Al-Dosari", 3.4, "Biomedical",
+    ["EE301", "EE370", "EE471", "EE250"],
+    user_email="s006@uni.edu")
+
+print("Done!")
+db.close()
